84965e0e6babe5312deb65d0d43881b18eac4b6e
Chris Beedie <chris.beedie@gmail.com>
2024-09-05 21:40:28 +0100

    prettier output

--- run.py
+++ run.py
@@ -4,10 +4,12 @@ from db import Db
 from gdrive import GoogleDrive
 from hivebrite import get_token, update_user
 from settings import settings
+from rich.console import Console
+from rich.table import Table
+from rich.live import Live
 
 db = Db(settings.db_path)
 gdrive = GoogleDrive()
-
 
 ## prep db
 # db.create_db()
@@ -22,48 +24,65 @@ gdrive = GoogleDrive()
 # https://drive.google.com/open?id=
 # wrong link 259310 - 1EYyhOnOMgCoGL4h17mr1bGoXuuXTU0gg
 
-for user in db.get_users():
+table = Table(title="Dream Big Australia Upload")
 
-    if not user.complete:
+table.add_column("User ID", style="cyan", no_wrap=True)
+table.add_column("First Name", style="magenta")
+table.add_column("Surname", style="magenta")
+table.add_column("CV", style="green")
+table.add_column("Headshot", style="green")
+table.add_column("Uploaded", style="green")
 
-        base_path = settings.get_year_path(user.year)
+console = Console()
 
-        # download files
-        if user.cv_id and not user.cv_path:
-            cv_path = gdrive.get_file(user.cv_id, base_path=base_path)
-            user.cv_path = str(cv_path)
-            db.commit()
 
-        if user.hs_id and not user.hs_path:
-            hs_path = gdrive.get_file(user.hs_id, base_path=base_path)
-            user.hs_path = str(hs_path)
-            db.commit()
+with Live(table, refresh_per_second=1) as live:  # update 4 times a second to feel fluid
 
-        cv_path = None
-        hs_path = None
+    for user in db.get_users():
 
-        if user.cv_path and not user.cv_loaded:
-            cv_path = Path(user.cv_path)
-            if not cv_path.exists():
-                raise KeyError(f"CV not found for: {user.user_id}")
+        live.console.print(f"Processing {user.user_id} - {user.first_name} {user.last_name}")
+
+        if not user.complete:
+
+            base_path = settings.get_year_path(user.year)
+
+            # download files
+            if user.cv_id and not user.cv_path:
+                cv_path = gdrive.get_file(user.cv_id, base_path=base_path)
+                user.cv_path = str(cv_path)
+                db.commit()
+
+            if user.hs_id and not user.hs_path:
+                hs_path = gdrive.get_file(user.hs_id, base_path=base_path)
+                user.hs_path = str(hs_path)
+                db.commit()
+
+            cv_path = None
+            hs_path = None
+
+            if user.cv_path and not user.cv_loaded:
+                cv_path = Path(user.cv_path)
+                if not cv_path.exists():
+                    raise KeyError(f"CV not found for: {user.user_id}")
+                
+            if user.hs_path and not user.hs_loaded:
+                hs_path = Path(user.hs_path)
+                if not hs_path.exists():
+                    raise KeyError(f"Headshot not found for: {user.user_id}")
+
+
+            if cv_path or hs_path:
+                success = update_user(user.user_id, cv_path, hs_path)
+
+                if cv_path and success:
+                    user.cv_loaded = True
+
+                if hs_path and success: 
+                    user.hs_loaded = True
+
+            if (not user.cv_id or user.cv_loaded) and (not user.hs_id or user.hs_loaded):
+                user.complete = True    
+
             
-        if user.hs_path and not user.hs_loaded:
-            hs_path = Path(user.hs_path)
-            if not hs_path.exists():
-                raise KeyError(f"Headshot not found for: {user.user_id}")
-
-
-        if cv_path or hs_path:
-            success = update_user(user.user_id, cv_path, hs_path)
-
-            if cv_path and success:
-                user.cv_loaded = True
-
-            if hs_path and success: 
-                user.hs_loaded = True
-
-        if (not user.cv_id or user.cv_loaded) and (not user.hs_id or user.hs_loaded):
-            user.complete = True    
-
-        print( user.user_id, user.first_name, user.last_name, "   > upload_success:", success, ", has_cv:", bool(cv_path), ", has_hs:", bool(hs_path)  )
-        db.commit()
+            table.add_row(str(user.user_id), user.first_name, user.last_name, str(bool(cv_path)), str(bool(hs_path)), str(success))
+            db.commit()
